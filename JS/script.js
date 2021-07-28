8dbe0ebf234bec2c5a3edd73144daadb1024b2b9
FrankLaurora <francesco.laurora9@gmail.com>
2021-07-28 18:02:53 +0200

    Fix bg image and script.js

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,11 @@
 // richiesta nome
-var firstName = prompt('Qual è il tuo nome?')
+var firstName = prompt('Qual è il tuo nome?');
 //richiesta cognome
-var lastName = prompt('Qual è il tuo cognome?')
+var lastName = prompt('Qual è il tuo cognome?');
 //richiesta colore
-var color = prompt('Qual è il tuo colore preferito?')
+var color = prompt('Qual è il tuo colore preferito?');
 //generazione password
-var pwd = firstName + lastName + color + "40"
-console.log(pwd)
+var pwd = firstName + lastName + color + "40";
+console.log(pwd);
 //scrittura in pagina
-document.getElementById('new_password').innerHTML = "La nostra nuova pass... ahem... la TUA nuova password è: " + pwd
+document.getElementById('new_password').innerHTML = "La nostra nuova pass... ahem... la TUA nuova password è: " + pwd;
