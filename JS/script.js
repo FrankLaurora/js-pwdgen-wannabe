a2f95feed906a92811ae938fca99a644931a743c
FrankLaurora <francesco.laurora9@gmail.com>
2021-07-28 16:18:52 +0200

    Add pwd generator

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+// richiesta nome
+var firstName = prompt('Qual è il tuo nome?')
+//richiesta cognome
+var lastName = prompt('Qual è il tuo cognome?')
+//richiesta colore
+var color = prompt('Qual è il tuo colore preferito?')
+//generazione password
+var pwd = firstName + lastName + color + "40"
+console.log(pwd)
+//scrittura in pagina
+document.getElementById('new_password').innerHTML = "La tua nuova password è: " + pwd
